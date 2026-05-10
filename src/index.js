ed74fc0781a80e704d86d792acd36a9a95c1af41
ohsanghy09 <ohsanghy09@naver.com>
2026-05-10 16:32:43 +0900

    comment : index.js에 주석 추가

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// App 컴포넌트 index.html과 연결해서 화면에 랜더링
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -7,7 +8,7 @@ import App from './App';
 // React 앱을 렌더링할 수 있는 루트 공간 생성 (도화지)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// root를 화면에 렌더링
+// root를 화면에 랜더링
 root.render(
   <React.StrictMode>
     <App />
